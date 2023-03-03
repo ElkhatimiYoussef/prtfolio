d081f4a7eb4519e14cc6322f323afc14a2c81656
ElkhatimiYoussef <khatimiyoussef211@gmail.com>
2023-03-03 23:38:16 +0100

    Your commit message here

--- src/sections/about/data.js
+++ src/sections/about/data.js
@@ -8,7 +8,7 @@ const data = [
         {id: 1, icon: <FaAward/>, title: 'Experience', desc: <big>building web application ( 2 month ) / stage d'été '</big>},
         {id: 2, icon: <TbBooks/>, title: 'Projects', desc: <big>3 professional project + Completed</big>},
         {id: 3, icon: <BiHappyHeartEyes/>, title: 'Certification', desc: <big>'15 + ( in progress of realisation)'</big>},
-        {id: 4, icon: <TbSchool/>, title: 'Current Formation', desc: <big>Je suis actuellement en quatrième année de formation d'ingénieur à l'École Marocaine des Sciences de l'Ingénieur (EMSI), spécialisé en ingénierie informatique et réseaux avec une option en méthodes informatiques appliquées à la gestion d'entreprise.'</big>},       
+        {id: 4, icon: <TbSchool/>, title: 'Current Formation', desc: 'Je suis étudiant en quatrième année à l\'École Marocaine des Sciences de l\'Ingénieur (EMSI) où j\'étudie l\'ingénierie informatique et réseaux. J\'ai choisi une option en méthodes informatiques appliquées à la gestion d\'entreprise'},       
         ]
 
 
